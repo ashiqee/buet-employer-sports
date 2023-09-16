af867fef0cf1b16d1172a7ebfb8569d0962e7067
ashiq73 <ashiq.buet73@gmail.com>
2023-09-16 18:28:06 +0600

    footer

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,21 @@
 import Header from "../Header/Header";
 import Banner from "./Banner";
 import Countdown from "./Countdown";
+import Footer from "./Footer";
 import Slider from "./Slider";
 
 const Home = () => {
   return (
     <div>
-      <Header />
-      <Banner />
-      <Countdown />
-      <Slider />
+      <div className="body">
+        {" "}
+        <Header />
+        <Banner />
+        <Countdown />
+        <Slider />
+      </div>
+
+      <Footer />
     </div>
   );
 };
