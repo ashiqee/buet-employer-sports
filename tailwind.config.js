ec0dab7914ca278dffc8cd5c5a5f45a5f0b0368a
ashiq73 <ashiq.buet73@gmail.com>
2023-09-24 14:37:41 +0600

    Player Profile View

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       "banner-img":
         "url('https://live.staticflickr.com/2500/3711782753_8123b71233_z.jpg')",
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), require("flowbite/plugin")],
 };
